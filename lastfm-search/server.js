a02e5a1f0fa4a929d1e6e8788ead0a1068058afe
notewok <eeva.sankala@gmail.com>
2019-05-19 21:48:07 +0300

    another fixed path

--- lastfm-search/server.js
+++ lastfm-search/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/lastfm-search'));
 
-app.get('/*', function(req,res) {
-    
+app.get('/*', function(req, res) {
+
 res.sendFile(path.join(__dirname+'/dist/lastfm-search/index.html'));
 });
 
