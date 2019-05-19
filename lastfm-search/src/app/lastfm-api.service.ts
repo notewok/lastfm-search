4298bf925586ed2cfb57341ce145688354e49536
notewok <eeva.sankala@gmail.com>
2019-05-19 22:06:00 +0300

    another fixed path

--- lastfm-search/src/app/lastfm-api.service.ts
+++ lastfm-search/src/app/lastfm-api.service.ts
@@ -22,13 +22,12 @@ export class LastfmApiService {
   storeTags(tags) {
     this.tags = tags.artist.tags.tag;
     for (let tag in this.tags) {
-      console.log("tags", this.tags[tag]);
     }
   }
 
   fetch(callBackFunction: (result) => void) {
     let httpAddress =
-    `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${this.input}&api_key=0ce3f03b4ae2c82ed37d595af73336b0&format=json`;
+    `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${this.input}&api_key=0ce3f03b4ae2c82ed37d595af73336b0&format=json`;
     this.http.get<LastfmData>(httpAddress).subscribe(jsonObject => {
       callBackFunction(jsonObject);
       this.storeTags(jsonObject);
@@ -37,7 +36,7 @@ export class LastfmApiService {
 
   fetchTags(callBackFunction: (result) => void) {
     let httpAddress =
-    `http://ws.audioscrobbler.com/2.0/?method=tag.getsimilar&tag=punk&api_key=0ce3f03b4ae2c82ed37d595af73336b0&format=json`;
+    `https://ws.audioscrobbler.com/2.0/?method=tag.getsimilar&tag=punk&api_key=0ce3f03b4ae2c82ed37d595af73336b0&format=json`;
     this.http.get<LastfmData>(httpAddress).subscribe(jsonObject => {
       callBackFunction(jsonObject);
     });
